ea05c28bb992c7683a7a874775c79447eb962576
leonkae <leon29kae@gmail.com>
2022-06-02 23:24:32 +0300

    installed pillow,modified templates and views

--- socials/models.py
+++ socials/models.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Profile(models.Models):
+    '''profile model'''
+    name = models.CharField(max_length=20, null=False,blank=False)
+    username = models.CharField(max_length=20,null=False,blank=False)
+    bio = models.charfield(max_length=255)
+    profilephoto = models.ImageField(upload_to='media/',default ='')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+   
+   
+    def __str__(self):
+       return self.name
+    
+
+class Post(models.Model):
+     '''image(post) model'''
+     image = models.ImageField(upload_to='media/',default='')
+     imagename = models.CharField(max_length=20)
+     caption = models.CharField(max_length=255)
+     user = models.ForeignKey(User, on_delete =models.CASCADE)
+     created = models.DateTimeField(auto_now_add=True)
+     modified =models.DateTimeField(auto_now_add=True)
+     profile =models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
+     
+
+class Comment(models.Model):
+    post =models.ForeignKey(Post, on_delete=models.CASCADE) 
+    comments = models.TextField(max_length=100) 
+    created = models.DateTimeField(auto_now_add=True)
+    modified =models.DateTimeField(auto_now_add=True)
--- socials/views.py
+++ socials/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 def login(request):
     return render(request, 'social/login.html')
 
 def signup(request):
-    return render(request, 'social/signup.html')
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            
+    context = {'form':form}
+    return render(request, 'social/signup.html', context)
